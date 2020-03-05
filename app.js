90b93dad5e05d69d7378e47ed6df70e055b0c771
Khadro Youssef - Komvux Kärnan <youssef.khadro@skola.helsingborg.se>
2020-03-05 15:48:34 +0100

    Added new line

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const form = document.querySelector('#form');
 const content = document.querySelector('#content');
 
-const age = prompt('Hur gammal är du? OBS: DU måste vara minst 20 år för att besöka sidan');
+const age = prompt('How old are you? You have to be atleast 20 years old to visit this website');
 
 // conditional Statement
 if(age >= 20) {
     // visa hemsidan
     content.style.display = 'block';
     const name = prompt('vad heter du');
-    document.querySelector('.message').innerHTML = "Välkommen" + name + "till hemsidan"
+    document.querySelector('.message').innerHTML = "Welcome" + name + "to website"
     document.querySelector('#enter').className = 'granted';
 } else {
     // inte gammal nog
     content.style.display = 'none';
-    document.querySelector('.message').innerHTML = 'Du är inte gammla nog, tyvärr.';
+    document.querySelector('.message').innerHTML = 'sorry, you are not old enough.';
     document.querySelector('#enter').className = 'denid';
 
 }
